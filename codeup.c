7176eb498f35428b891a8951807f0f2c82684901
1140r <100419156+1140r@users.noreply.github.com>
2022-03-08 20:39:17 +0900

    !!

---
+++ codeup.c
@@ -0,0 +1,8 @@
+//
+//  codeup.c
+//  FirstProject
+//
+//  Created by ruma lee on 2021/06/01.
+//
+
+#include "codeup.h"
---
+++ codeup.h
@@ -0,0 +1,13 @@
+//
+//  codeup.h
+//  FirstProject
+//
+//  Created by ruma lee on 2021/06/01.
+//
+
+#ifndef codeup_h
+#define codeup_h
+
+#include <stdio.h>
+
+#endif /* codeup_h */
